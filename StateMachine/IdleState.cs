669fae458a5b1d4bee3d9bc4caa1580d0a2af934
Damon S. Green II <damonsgreen2@gmail.com>
2025-02-21 13:57:21 -0500

    Add project files.

---
+++ StateMachine/IdleState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace IuvoUnity
+{
+    namespace IuvoStateMachine
+    {
+        [CreateAssetMenu(fileName = "IdleState", menuName = "StateMachine/States/IdleState")]
+        public class IdleState : State
+        {
+            public override void OnEnter(StateMachine stateMachine)
+            {
+                Debug.Log("Entering Idle State");
+
+            }
+
+            public override void OnUpdate(StateMachine stateMachine)
+            {
+
+            }
+
+            public override void OnFixedUpdate(StateMachine stateMachine)
+            {
+
+            }
+
+            public override void OnExit(StateMachine stateMachine)
+            {
+                Debug.Log("Exiting Idle State");
+
+            }
+        }
+
+
+    }
+
+}
---
+++ StateMachine/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace IuvoUnity
+{
+    namespace IuvoStateMachine
+    {
+        [System.Serializable]
+        public abstract class State : ScriptableObject
+        {
+            public string stateName;
+
+            public virtual bool CanEnter(StateMachine stateMachine)
+            {
+                return true;
+            }
+            public abstract void OnEnter(StateMachine stateMachine);
+            public abstract void OnUpdate(StateMachine stateMachine);
+            public abstract void OnFixedUpdate(StateMachine stateMachine);
+            public abstract void OnExit(StateMachine stateMachine);
+        }
+    }
+}
