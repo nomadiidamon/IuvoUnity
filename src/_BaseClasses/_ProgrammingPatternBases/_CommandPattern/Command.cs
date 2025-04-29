750df083dbdff8917ae16acd11407da84fec2fc3
Damon S. Green II <damonsgreen2@gmail.com>
2025-04-29 02:57:25 -0400

    namespace structure layout document added to src folder

---
+++ src/_BaseClasses/_ProgrammingPatternBases/_CommandPattern/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IuvoUnity
+{
+    namespace _BaseClasses
+    {
+        public abstract class Command
+        {
+            public abstract void Execute();
+            public abstract void Undo();
+        }
+    }
+}
---
+++ src/_DataStructs/RangeDataComponent.cs
@@ -0,0 +1,25 @@
+﻿
+namespace IuvoUniy
+{
+
+    namespace _DataStructs
+    {
+        public struct RangeFloat
+        {
+            public float min;
+            public float max;
+            public RangeFloat(float _min, float _max)
+            {
+                min = _min;
+                max = _max;
+            }
+
+            public float RandomInRange()
+            {
+                return UnityEngine.Random.Range(min, max);
+            }
+        }
+
+
+    }
+}
