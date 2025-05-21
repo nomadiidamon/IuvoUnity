1e253195c762c0a831e84b8edd4f2cfad48ae8e5
Damon S. Green II <damonsgreen2@gmail.com>
2025-05-21 14:00:48 -0400

    updated references for input and cinemachine

--- src/_BaseClasses/_InputBases/T_InputBase.cs
+++ src/_BaseClasses/_InputBases/T_InputBase.cs
@@ -1,52 +1,52 @@
-﻿//using UnityEngine;
-//using UnityEngine.Events;
-//using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-//namespace IuvoUnity
-//{
+namespace IuvoUnity
+{
 
-//    namespace _BaseClasses
-//    {
+    namespace _BaseClasses
+    {
 
-//        namespace _InputBases
-//        {
-//            public abstract class InputActionBase : MonoBehaviour
-//            {
-//                public UnityEvent OnPerformedUnity;
-//                public System.Action OnPerformed;
+        namespace _InputBases
+        {
+            public abstract class InputActionBase : MonoBehaviour
+            {
+                public UnityEvent OnPerformedUnity;
+                public System.Action OnPerformed;
 
-//                public InputAction inputAction;
+                public InputAction inputAction;
 
-//                protected virtual void OnEnable()
-//                {
-//                    if (inputAction != null)
-//                    {
-//                        inputAction.Enable();
-//                        inputAction.performed += OnInputPerformed;
-//                    }
-//                }
+                protected virtual void OnEnable()
+                {
+                    if (inputAction != null)
+                    {
+                        inputAction.Enable();
+                        inputAction.performed += OnInputPerformed;
+                    }
+                }
 
-//                protected virtual void OnDisable()
-//                {
-//                    if (inputAction != null)
-//                    {
-//                        inputAction.performed -= OnInputPerformed;
-//                        inputAction.Disable();
-//                    }
-//                }
+                protected virtual void OnDisable()
+                {
+                    if (inputAction != null)
+                    {
+                        inputAction.performed -= OnInputPerformed;
+                        inputAction.Disable();
+                    }
+                }
 
-//                protected virtual void OnInputPerformed(InputAction.CallbackContext context)
-//                {
-//                    Perform();
-//                }
+                protected virtual void OnInputPerformed(InputAction.CallbackContext context)
+                {
+                    Perform();
+                }
 
-//                protected void Perform()
-//                {
-//                    Debug.Log($"{name}: Performed!");
-//                    OnPerformedUnity?.Invoke();
-//                    OnPerformed?.Invoke();
-//                }
-//            }
-//        }
-//    }
-//}
+                protected void Perform()
+                {
+                    Debug.Log($"{name}: Performed!");
+                    OnPerformedUnity?.Invoke();
+                    OnPerformed?.Invoke();
+                }
+            }
+        }
+    }
+}
